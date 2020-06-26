c9d0389a8ff7386cb31586bb1c6d62e422a18e25
Abdulkadir Şen <abdulkadir.sen@neyasis.com>
2020-06-26 13:21:35 +0300

    initial create

---
+++ src/Crossxox/Crossxox/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("DISCO.ttf", Alias = "Default")]
---
+++ src/Crossxox/Crossxox/GamePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Crossxox
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GamePage : ContentPage
+    {
+        public GamePage()
+        {
+            InitializeComponent();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            App.Current.MainPage = new MainPage();
+
+            return true;
+        }
+    }
+}
